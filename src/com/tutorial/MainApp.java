2bb44e63f912e087f8716e1bb9c4f35b67b871f0
Rasper4o <gsimeonov28@gmail.com>
2017-08-18 22:03:26 -0400

    usage of singleton scope beans

--- src/com/tutorial/MainApp.java
+++ src/com/tutorial/MainApp.java
@@ -8,8 +8,13 @@ public class MainApp {
 	public static void main(String[] args)
 	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld object=(HelloWorld) context.getBean("helloWorld");
+		HelloWorld object1=(HelloWorld) context.getBean("helloWorld");
 		
-		object.getMessage();
+		object1.setMessage("I am object 1");
+		object1.getMessage();
+		
+		HelloWorld object2 = (HelloWorld) context.getBean("helloWorld");
+		
+		object2.getMessage();
 	}
 }
