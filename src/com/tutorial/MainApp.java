db7e92513a4a17504e3df11f43873be1fecac849
Rasper4o <gsimeonov28@gmail.com>
2017-08-20 00:45:33 -0400

    demonstrating dependency injection; injecting inner beans

--- src/com/tutorial/MainApp.java
+++ src/com/tutorial/MainApp.java
@@ -8,17 +8,9 @@ public class MainApp {
 
 	public static void main(String[] args)
 	{
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
-		HelloWorld object1=(HelloWorld) context.getBean("helloWorld");
-		object1.getMessage1();
-		object1.getMessage2();
-		
-		System.out.println();
-		
-		HelloBulgaria object2 = (HelloBulgaria) context.getBean("helloBulgaria");
-		object2.getMessage1();
-		object2.getMessage2();
-		object2.getMessage3();
+		TextEditor textEditor = (TextEditor) context.getBean("textEditor");
+		textEditor.spellCheck();
 	}
 }
---
+++ src/com/tutorial/TextEditor.java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+public class TextEditor {
+	
+	private SpellChecker spellChecker;
+	
+	public void setSpellChecker(SpellChecker spellChecker)
+	{
+		System.out.println("Inside setSpellChecker");
+		this.spellChecker = spellChecker;
+	}
+	
+	public SpellChecker getSpellChecker()
+	{
+		return spellChecker;
+	}
+	
+	public void spellCheck()
+	{
+		spellChecker.checkSpelling();
+	}
+}
